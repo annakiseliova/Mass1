65d4b5b39b9338e877df4595ff1d69d62869c312
annakiseliova <annakiseliova7@gmail.com>
2022-10-07 17:28:41 +0300

    Массивы. Часть 1. Задание 4

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,16 @@ public class Main {
         double[] mass2 = {1.57, 7.654, 9.986};
         int[] mass3 = {5, 89, 65, 77, 23, 32};
 
-        // Массивы. Часть 1. Задание 3
-        System.out.println("Массивы. Часть 1. Задание 3");
-
-        for (int i = mass.length-1; i >= 0; i--) {
+        // Массивы. Часть 1. Задание 4
+        System.out.println("Массивы. Часть 1. Задание 4");
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] % 2 != 0) {
+                mass[i] = mass[i] + 1;
+            }
             System.out.print(mass[i]);
-            if (i != 0) {
-                System.out.print(", ");
+            if (i != mass.length - 1) {
+                System.out.print(", ");;
             }
         }
-        System.out.println();
-        for (int a = mass2.length-1; a >= 0; a--) {
-            System.out.print(mass2[a]);
-            if (a != 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println();
-        for (int b = mass3.length-1; b >= 0; b--) {
-            System.out.print(mass3[b]);
-            if (b != 0) {
-                System.out.print(", ");
-            }
-        }
-
-
     }
 }
